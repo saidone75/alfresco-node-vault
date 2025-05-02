aafd2b732213a91b480bfc9a4c5f27648f09ff49
Marco Marini <mmarini@tai.it>
2025-05-02 10:23:30 +0200

    Add CustomMetrics class to register a gauge for counting nodes in MongoDB vault collection

---
+++ src/main/java/org/saidone/monitor/CustomMetrics.java
@@ -0,0 +1,28 @@
+package org.saidone.monitor;
+
+import io.micrometer.core.instrument.Gauge;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Tags;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Component;
+
+//@Component
+public class CustomMetrics {
+
+    private final MeterRegistry meterRegistry;
+    private final MongoTemplate mongoTemplate;
+
+    public CustomMetrics(MeterRegistry meterRegistry, MongoTemplate mongoTemplate) {
+        this.meterRegistry = meterRegistry;
+        this.mongoTemplate = mongoTemplate;
+        Gauge.builder("nodes_on_vault", this, CustomMetrics::getAlfNodeCount)
+                .description("Nodes on vault")
+                .tags(Tags.of("vault", "nodes"))
+                .register(meterRegistry);
+    }
+
+    private long getAlfNodeCount() {
+        return mongoTemplate.getCollection("alf_node").countDocuments();
+    }
+
+}
