6492519f9992da6c770d0dc7fc0e391902e47785
Marco Marini <mmarini@tai.it>
2025-05-04 14:22:02 +0200

    Refactor massiveLoadTest to use fixed runtime and simplify task loop

--- src/test/java/org/saidone/test/LoadTests.java
+++ src/test/java/org/saidone/test/LoadTests.java
@@ -15,11 +15,9 @@ import org.springframework.test.context.ActiveProfiles;
 
 import java.net.URI;
 import java.util.List;
-import java.util.Objects;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
@@ -33,10 +31,6 @@ class LoadTests extends BaseTest {
     VaultService vaultService;
     @Autowired
     private AlfrescoService alfrescoService;
-
-    private int getChildrenCount() {
-        return Objects.requireNonNull(nodesApi.listNodeChildren(parentId, 0, Integer.MAX_VALUE, null, null, null, null, null, null).getBody()).getList().getEntries().size();
-    }
 
     @Test
     @Tag("load")
@@ -63,42 +57,23 @@ class LoadTests extends BaseTest {
     @Tag("massive")
     @SneakyThrows
     void massiveLoadTest() {
-        val url = (URI.create("https://freetestdata.com/wp-content/uploads/2021/09/Free_Test_Data_100KB_PDF.pdf").toURL());
-        val batchSize = new AtomicInteger(0);
-
-        @Cleanup var executor = Executors.newFixedThreadPool(4);
-
-        val paused = new AtomicBoolean(false);
-
+        val url = URI.create("https://freetestdata.com/wp-content/uploads/2021/09/Free_Test_Data_100KB_PDF.pdf").toURL();
+        @Cleanup val executor = Executors.newFixedThreadPool(4);
+        val running = new AtomicBoolean(true);
         Runnable task = () -> {
-            try {
-                while (true) {
-                    if (!paused.get()) {
-                        val nodeId = createNode(url).getId();
-                        log.debug("Node created: {}", nodeId);
-                        alfrescoService.addAspects(nodeId, List.of(AnvContentModel.ASP_ARCHIVE));
-                        batchSize.incrementAndGet();
-                    }
-                    if (batchSize.get() >= 1000) {
-                        val childrenCount = getChildrenCount();
-                        batchSize.set(childrenCount);
-                        if (childrenCount > 1000) {
-                            log.debug("Children count: {} - Pausing for 10 seconds...", childrenCount);
-                            paused.set(true);
-                            TimeUnit.SECONDS.sleep(10);
-                        }
-                    } else {
-                        paused.set(false);
-                    }
-                }
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+            while (running.get()) {
+                val nodeId = createNode(url).getId();
+                log.debug("Node created: {}", nodeId);
+                alfrescoService.addAspects(nodeId, List.of(AnvContentModel.ASP_ARCHIVE));
             }
         };
 
         for (int i = 0; i < 8; i++) {
             executor.submit(task);
         }
+
+        TimeUnit.HOURS.sleep(1);
+        running.set(false);
     }
 
 }
