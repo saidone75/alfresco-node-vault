f30c11567a2aea43fea43898fe33f27ddd1c99a7
Marco Marini <mmarini@tai.it>
2025-07-07 09:00:17 +0200

    Add GPL license headers to test classes and extend `TestCleanupService` with S3 bucket cleanup logic

--- src/test/java/org/saidone/test/TestCleanupService.java
+++ src/test/java/org/saidone/test/TestCleanupService.java
@@ -1,3 +1,21 @@
+/*
+ *  Alfresco Node Vault - archive today, accelerate tomorrow
+ *  Copyright (C) 2025 Saidone
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.saidone.test;
 
 import com.mongodb.client.MongoClient;
@@ -28,6 +46,9 @@ public class TestCleanupService {
 
     @Value("${spring.data.mongodb.database}")
     private String database;
+
+    @Value("${application.service.vault.storage.s3.bucket}")
+    private String bucketName;
 
     @Setter
     private static String parentId;
@@ -61,6 +82,7 @@ public class TestCleanupService {
         if (parentId != null) {
             nodesApi.deleteNode(parentId, true);
             mongoClient.getDatabase(database).drop();
+            if (s3Client != null) emptyBucket(bucketName);
         }
     }
 
