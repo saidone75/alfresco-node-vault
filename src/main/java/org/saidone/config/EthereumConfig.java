45cb5df2cf3d38179a8ec83890c2ab677ba1ae62
Marco Marini <mmarini@tai.it>
2025-07-10 09:17:49 +0200

    Refactor `EthereumConfig`: replace `System.out` with `Slf4j` logging, use `val` for key pair, and clean up unused imports.

--- src/main/java/org/saidone/config/EthereumConfig.java
+++ src/main/java/org/saidone/config/EthereumConfig.java
@@ -2,10 +2,10 @@ package org.saidone.config;
 
 import jakarta.annotation.PostConstruct;
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.web3j.crypto.ECKeyPair;
 import org.web3j.crypto.Keys;
 import org.web3j.utils.Numeric;
 
@@ -16,6 +16,7 @@ import java.security.NoSuchProviderException;
 @Configuration
 @ConfigurationProperties(prefix = "application.service.ethereum")
 @Data
+@Slf4j
 public class EthereumConfig {
 
     private String rpcUrl;
@@ -26,17 +27,10 @@ public class EthereumConfig {
     @PostConstruct
     public void init() throws InvalidAlgorithmParameterException, NoSuchAlgorithmException, NoSuchProviderException {
      if (autoGenerate) {
-         ECKeyPair keyPair = Keys.createEcKeyPair();
-         String privateKey = Numeric.toHexStringWithPrefix(keyPair.getPrivateKey());
-         String address = "0x" + Keys.getAddress(keyPair.getPublicKey());
-
-         this.account = address;
-         this.privateKey = privateKey;
-
-         System.out.println("Account Ethereum generato automaticamente:");
-         System.out.println("Indirizzo: " + address);
-
-
+         val keyPair = Keys.createEcKeyPair();
+         this.privateKey = Numeric.toHexStringWithPrefix(keyPair.getPrivateKey());
+         this.account = String.format("0x%s", Keys.getAddress(keyPair.getPublicKey()));
+         log.debug("Ethereum account automatically generated: {}", this.account);
      }
     }
 
