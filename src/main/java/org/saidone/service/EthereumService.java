e495b3e781b051601880d4a663c5108f8f277c45
Marco Marini <mmarini@tai.it>
2025-07-10 09:10:49 +0200

    Refactor `EthereumService`: improve transaction management by adding chain ID support, optimize imports, and adjust gas price and limit placeholders.

--- src/main/java/org/saidone/service/EthereumService.java
+++ src/main/java/org/saidone/service/EthereumService.java
@@ -7,18 +7,15 @@ import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.saidone.component.BaseComponent;
 import org.saidone.config.EthereumConfig;
+import org.springframework.stereotype.Service;
 import org.web3j.crypto.Credentials;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.http.HttpService;
-import org.web3j.protocol.core.methods.response.EthSendTransaction;
 import org.web3j.tx.RawTransactionManager;
-import org.web3j.tx.TransactionManager;
-import org.web3j.tx.gas.DefaultGasProvider;
 import org.web3j.utils.Numeric;
-import org.springframework.stereotype.Service;
 
-import java.nio.charset.StandardCharsets;
 import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Service responsible for interacting with an Ethereum node to store document hashes.
@@ -61,11 +58,14 @@ public class EthereumService extends BaseComponent {
      */
     public String storeHash(String nodeId, String hash) {
         try {
-            TransactionManager txManager = new RawTransactionManager(web3j, credentials);
-            String data = Numeric.toHexString(hash.getBytes(StandardCharsets.UTF_8));
-            EthSendTransaction tx = txManager.sendTransaction(
-                    DefaultGasProvider.GAS_PRICE,
-                    DefaultGasProvider.GAS_LIMIT,
+            val chainId = web3j.ethChainId().send().getChainId();
+            val txManager = new RawTransactionManager(web3j, credentials, chainId.longValue());
+            val data = Numeric.toHexString(hash.getBytes(StandardCharsets.UTF_8));
+            val tx = txManager.sendTransaction(
+                    // FIXME get gas price
+                    BigInteger.ZERO,
+                    // FIXME estimate gas limit
+                    BigInteger.valueOf(50000L),
                     config.getAccount(),
                     data,
                     BigInteger.ZERO);
@@ -77,4 +77,5 @@ public class EthereumService extends BaseComponent {
             throw new RuntimeException(e);
         }
     }
+
 }
