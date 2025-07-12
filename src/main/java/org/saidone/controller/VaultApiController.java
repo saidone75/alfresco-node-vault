1db25d1b2098276a2e9b91ee0ad72a6ce08d2c01
saidone <saidone@saidone.org>
2025-07-12 08:22:05 +0200

    Remove unused import java.util.UUID from VaultApiController.java

--- src/main/java/org/saidone/controller/VaultApiController.java
+++ src/main/java/org/saidone/controller/VaultApiController.java
@@ -44,7 +44,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
 /**
