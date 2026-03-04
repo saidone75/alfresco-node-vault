bca7b1e7ff33c3dad8e045738147c9bc7aee250d
saidone <saidone@saidone.org>
2026-03-04 08:41:27 +0100

    Use wildcard when copying built JAR to DIST_DIR to handle versioned filenames

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ if [ -e $DIST_DIR ]; then rm -rf $DIST_DIR; fi
 mkdir -p $DIST_DIR/log
 mkdir -p $DIST_DIR/config
 mvn package -DskipTests -Dlicense.skip=true
-cp target/anv.jar $DIST_DIR
+cp target/anv*.jar $DIST_DIR
 cp src/main/resources/application.yml $DIST_DIR/config
